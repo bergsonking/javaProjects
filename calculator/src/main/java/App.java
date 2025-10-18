14a7920b14211cf12ca8f3cf8f93d45b5fc77241
erenyavuz <101049103+bergsonking@users.noreply.github.com>
2025-10-18 23:12:59 +0200

    Add files via upload

---
+++ calculator/src/main/java/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class App {
+  /* Bu proje ile amacim basit bir hesap makinesi gelistirmek
+     Kullanicidan alinan girdiler olacak
+     Kullanicinin yapmak istedigi islem kullanicidan alinacak
+     Sonuc ekrana yazdirilacak
+   */
+   public static void main(String[] args) {
+     Calculator calculator = new Calculator();
+    }
+}
